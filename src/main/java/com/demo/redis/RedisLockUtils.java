744c4ef7695dce36a18942fb9f1bc6252653d271
YiHaoxing <m15670388498>
2019-09-04 16:24:58 +0800

    修改LUA脚本,之前的获取锁的脚本存在错误,会一直返回1

--- src/main/java/com/demo/redis/RedisLockUtils.java
+++ src/main/java/com/demo/redis/RedisLockUtils.java
@@ -78,7 +78,7 @@ public class RedisLockUtils {
     /**
      * 获取锁的LUA脚本：用setNx命令设置值,并设置过期时间
      */
-    public static final String LOCK_SCRIPT_LUA = "if redis.call('setNx',KEYS[1],ARGV[1]) then if redis.call('get',KEYS[1])==ARGV[1] then return redis.call('expire',KEYS[1],ARGV[2]) else return 0 end end";
+    public static final String LOCK_SCRIPT_LUA = "if redis.call('setNx',KEYS[1],ARGV[1]) == 1 then return redis.call('expire',KEYS[1],ARGV[2]) else return 0 end";
 
     /**
      * @param [lockKey, value, expireTime]
